76f6c870e3b14bbaf4c4a15eb34000b5f72122b2
KessaPassa <k.someya.gyao@gmail.com>
2016-07-14 17:29:09 +0900

    第二弾

--- Assets/counter.cs
+++ Assets/counter.cs
@@ -3,18 +3,25 @@ using System.Collections;
 
 public class counter : MonoBehaviour {
     public int count = 0;
-
+    private int i = 0;
 	
 	void Start () {
-	    for(int i = 1; i < count; i++)
+        //第一弾
+	    /*for(int i = 1; i < count; i++)
         {
             if(i % 2 == 0)
                 Debug.Log(i);
-        }
+        }*/
 	}
 	
 	
 	void Update () {
-	    
-	}
+        //第二弾
+        if (i == count)
+            return;
+
+        i++;
+        if (i % 2 == 0)
+            Debug.Log(i);
+    }
 }
