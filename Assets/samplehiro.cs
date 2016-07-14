d2d1d7313af1df766354f7b2bfdef4ebe554f8e9
unknown <>
2016-07-14 17:31:01 +0900

    samplehiro

---
+++ Assets/samplehiro.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class samplehiro : MonoBehaviour {
+	public int n;
+	int i=0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (i < n) {
+			print(i * 2);
+			i++;
+		}
+	}
+}
