694618b31efadd7fd11095fa162e5a21f42e73f8
furuhiro <kanakana1610@gmail.com>
2016-07-14 17:40:11 +0900

    Asset/hogeを追加

---
+++ Assets/hoge.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hoge : MonoBehaviour
+{
+    public int n;
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i <= n; i++)
+        {
+            if (i%2==0)
+            {
+                print(i);
+            }
+        }
+    }
+}
