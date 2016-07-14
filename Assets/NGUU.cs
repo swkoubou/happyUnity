1f9ffc313a6fea0ef0266d17249fb242f6ec4f3b
goto <gotomsak@gmail.com>
2016-07-14 18:35:40 +0900

    nyannpasu

---
+++ Assets/NGUU.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NGUU : MonoBehaviour {
+   
+    public int a;
+    int b=0;
+	// Use this for initialization
+	void Start () {
+        for (b = 0; b <= a; b++) {
+            if (b % 2 == 0)
+            {
+                Debug.Log(b);
+            }
+       }
+    }
+}
