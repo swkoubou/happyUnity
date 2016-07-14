90fda2287b924d425e28e948d556042b441098a0
kazu-mottan <kaitoxtennis1997mail@gmail.com>
2016-07-14 17:55:07 +0900

    hogehoge.cs

--- Assets/hoge/hogehoge.cs
+++ Assets/hoge/hogehoge.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class hogehoge : MonoBehaviour {
-
+    
+    
 	// Use this for initialization
 	void Start () {
-        print("頑張ってね");
+        var i = 0;
+        var a = 101;
+        for (i = a; i >= 0; i = i - 2)
+        {
+            if (i % 2 != 0)
+            {
+                i--;
+            }
+            print(i);
+        }
+        
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frameS
 	void Update () {
-	
+        
 	}
 }
