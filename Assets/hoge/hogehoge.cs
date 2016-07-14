84786148fe0273fae79efe3357c492b5e7c33de1
haratatsurou <hara.tatsurou@gmail.com>
2016-07-14 16:23:59 +0900

    サンプル追加

---
+++ Assets/hoge/hogehoge.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hogehoge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("頑張ってね");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
